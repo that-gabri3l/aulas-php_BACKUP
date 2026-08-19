f9de36308e6353462d1fa611ebb85ddb00750f9b
that-gabri3l <gabrielalanpardinho123@gmail.com>
2026-08-19 11:38:19 -0300

    Separa cabecalho e rodape

---
+++ site/includes/cabecalho.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site usando PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+    <header>
+        <h1>Site com php</h1>
+        <nav>
+            <a href="index.php">Inicial</a>
+            <a href="cursos.php">Cursos</a>
+            <a href="duvidas.php">Duvidas</a>
+        </nav>
+    </header>
+
+    <main>
---
+++ site/index.php
@@ -0,0 +1,7 @@
+<?php require "includes/cabecalho.php" ?>
+
+        <h2>Bem-Vindo ao site de Exemplo</h2>
+        <p>Esta é a primeira pagina do nosso Site</p>
+    </main>
+
+<?php require "includes/rodape.php" ?>
